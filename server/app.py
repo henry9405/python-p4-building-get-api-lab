a291c12a78a4527111371a7068c0b419255a60c7
henry9405 <charleshenry9405@gmail.com>
2024-01-15 13:00:11 +0300

    new

--- server/app.py
+++ server/app.py
@@ -1,3 +1,42 @@
+# #!/usr/bin/env python3
+
+# from flask import Flask, make_response, jsonify
+# from flask_migrate import Migrate
+
+# from models import db, Bakery, BakedGood
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.json.compact = False
+
+# migrate = Migrate(app, db)
+
+# db.init_app(app)
+
+# @app.route('/')
+# def index():
+#     return '<h1>Bakery GET API</h1>'
+
+# @app.route('/bakeries')
+# def bakeries():
+#     return ''
+
+# @app.route('/bakeries/<int:id>')
+# def bakery_by_id(id):
+#     return ''
+
+# @app.route('/baked_goods/by_price')
+# def baked_goods_by_price():
+#     return ''
+
+# @app.route('/baked_goods/most_expensive')
+# def most_expensive_baked_good():
+#     return ''
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
+
 #!/usr/bin/env python3
 
 from flask import Flask, make_response, jsonify
@@ -20,19 +59,32 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    all_bakeries = Bakery.query.all()
+    bakery_list = [{'id': bakery.id, 'name': bakery.name, 'baked_goods': [good.serialize() for good in bakery.baked_goods]} for bakery in all_bakeries]
+    return jsonify(bakery_list)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    if bakery:
+        bakery_data = {'id': bakery.id, 'name': bakery.name, 'baked_goods': [good.serialize() for good in bakery.baked_goods]}
+        return jsonify(bakery_data)
+    else:
+        return jsonify({'message': 'Bakery not found'}), 404
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_list = [good.serialize() for good in baked_goods]
+    return jsonify(baked_goods_list)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    if most_expensive_good:
+        return jsonify(most_expensive_good.serialize())
+    else:
+        return jsonify({'message': 'No baked goods found'}), 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
